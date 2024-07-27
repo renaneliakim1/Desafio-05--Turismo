7f1b9d370ba96ef8210dae91fd3f11dafba85692
renaneliakim1 <renaneliakim1@gmail.com>
2024-07-26 21:21:35 -0300

    trying to develop search in db

--- app.js
+++ app.js
@@ -1,30 +1,33 @@
-const express = require('express');
-const path = require('path');
-const pool = require('./db');
+const express = require("express");
+const path = require("path");
+const pool = require("./db");
 const app = express();
 const port = 3000;
 
-app.set('views', path.join(__dirname, 'views')); // Diretório de visualizações
-app.set('view engine', 'ejs');
+app.set("views", path.join(__dirname, "views")); // Diretório de visualizações
+app.set("view engine", "ejs");
 
-app.use(express.static(path.join(__dirname, 'public'))); // Diretório de arquivos estáticos (CSS, imagens, etc.)
+app.use(express.static(path.join(__dirname, "public"))); // Diretório de arquivos estáticos (CSS, imagens, etc.)
 
 // Rota principal
-app.get('/', async (req, res) => {
-    try {
-        const destinosResult = await pool.query('SELECT * FROM Destino');
-        const destinos = destinosResult.rows;
+app.get("/", async (req, res) => {
+  try {
+    const destinosResult = await pool.query("SELECT * FROM Destino");
+    const destinos = destinosResult.rows;
 
-        for (let destino of destinos) {
-            const atrativosResult = await pool.query('SELECT * FROM Atrativo WHERE destino_id = $1', [destino.id]);
-            destino.atrativos = atrativosResult.rows;
-        }
-
-        res.render('index', { destinos: destinos });
-    } catch (err) {
-        console.error(err.message);
-        res.status(500).send('Erro no servidor');
+    for (let destino of destinos) {
+      const atrativosResult = await pool.query(
+        "SELECT * FROM Atrativo WHERE destino_id = $1",
+        [destino.id]
+      );
+      destino.atrativos = atrativosResult.rows;
     }
+
+    res.render("index", { destinos: destinos });
+  } catch (err) {
+    console.error(err.message);
+    res.status(500).send("Erro no servidor");
+  }
 });
 
 // Rota para inglês
@@ -45,46 +48,75 @@ app.get('/', async (req, res) => {
     }
 }); */
 
+app.get("/en", async (req, res) => {
+  try {
+    const destinosResult = await pool.query("SELECT * FROM Destino");
+    const destinos = destinosResult.rows;
 
-app.get('/en', async (req, res) => {
-    try {
-        const destinosResult = await pool.query('SELECT * FROM Destino');
-        const destinos = destinosResult.rows;
-
-        for (let destino of destinos) {
-            const atrativosResult = await pool.query('SELECT * FROM Atrativo WHERE destino_id = $1', [destino.id]);
-            destino.atrativos = atrativosResult.rows;
-        }
-
-        res.render('en', { destinos: destinos });
-    } catch (err) {
-        console.error(err.message);
-        res.status(500).send('Erro no servidor');
+    for (let destino of destinos) {
+      const atrativosResult = await pool.query(
+        "SELECT * FROM Atrativo WHERE destino_id = $1",
+        [destino.id]
+      );
+      destino.atrativos = atrativosResult.rows;
     }
+
+    res.render("en", { destinos: destinos });
+  } catch (err) {
+    console.error(err.message);
+    res.status(500).send("Erro no servidor");
+  }
 });
 
-
-
-
-
 // Rota para espanhol
-app.get('/es', async (req, res) => {
-    try {
-        const destinosResult = await pool.query('SELECT * FROM Destino');
-        const destinos = destinosResult.rows;
+app.get("/es", async (req, res) => {
+  try {
+    const destinosResult = await pool.query("SELECT * FROM Destino");
+    const destinos = destinosResult.rows;
 
-        for (let destino of destinos) {
-            const atrativosResult = await pool.query('SELECT * FROM Atrativo WHERE destino_id = $1', [destino.id]);
-            destino.atrativos = atrativosResult.rows;
-        }
-
-        res.render('es', { destinos: destinos }); // Certifique-se de que este arquivo está na pasta 'views'
-    } catch (err) {
-        console.error(err.message);
-        res.status(500).send('Erro no servidor');
+    for (let destino of destinos) {
+      const atrativosResult = await pool.query(
+        "SELECT * FROM Atrativo WHERE destino_id = $1",
+        [destino.id]
+      );
+      destino.atrativos = atrativosResult.rows;
     }
+
+    res.render("es", { destinos: destinos }); // Certifique-se de que este arquivo está na pasta 'views'
+  } catch (err) {
+    console.error(err.message);
+    res.status(500).send("Erro no servidor");
+  }
 });
 
 app.listen(port, () => {
-    console.log(`Server running on port ${port}`);
+  console.log(`Server running on port ${port}`);
+});
+
+
+
+//buscar no banco de dados
+
+app.get("/search", async (req, res) => {
+  const searchQuery = req.query.q;
+  try {
+    const destinosResult = await pool.query(
+      `SELECT * FROM Destino WHERE nome ILIKE $1 OR descricao ILIKE $1`,
+      [`%${searchQuery}%`]
+    );
+    
+    const destinos = destinosResult.rows;
+    for (let destino of destinos) {
+      const atrativosResult = await pool.query(
+        `SELECT * FROM Atrativo WHERE destino_id = $1 AND (nome ILIKE $2 OR descricao ILIKE $2)`,
+        [destino.id, `%${searchQuery}%`]
+      );
+      destino.atrativos = atrativosResult.rows;
+    }
+
+    res.render("search", { destinos: destinos, query: searchQuery });
+  } catch (err) {
+    console.error(err.message);
+    res.status(500).send("Erro no servidor");
+  }
 });
