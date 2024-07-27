dbeead67d44e4e546fef174a18e29cfccfa93d59
renaneliakim1 <renaneliakim1@gmail.com>
2024-07-26 22:03:22 -0300

    "search"

--- app.js
+++ app.js
@@ -4,12 +4,11 @@ const pool = require("./db");
 const app = express();
 const port = 3000;
 
-app.set("views", path.join(__dirname, "views")); // Diretório de visualizações
+app.set("views", path.join(__dirname, "views"));
 app.set("view engine", "ejs");
 
-app.use(express.static(path.join(__dirname, "public"))); // Diretório de arquivos estáticos (CSS, imagens, etc.)
+app.use(express.static(path.join(__dirname, "public")));
 
-// Rota principal
 app.get("/", async (req, res) => {
   try {
     const destinosResult = await pool.query("SELECT * FROM Destino");
@@ -23,30 +22,12 @@ app.get("/", async (req, res) => {
       destino.atrativos = atrativosResult.rows;
     }
 
-    res.render("index", { destinos: destinos });
+    res.render("index", { destinos });
   } catch (err) {
     console.error(err.message);
     res.status(500).send("Erro no servidor");
   }
 });
-
-// Rota para inglês
-/* app.get('/en', async (req, res) => {
-    try {
-        const destinosResult = await pool.query('SELECT * FROM Destino');
-        const destinos = destinosResult.rows;
-
-        for (let destino of destinos) {
-            const atrativosResult = await pool.query('SELECT * FROM Atrativo WHERE destino_id = $1', [destino.id]);
-            destino.atrativos = atrativosResult.rows;
-        }
-
-        res.render('en', { destinos: destinos }); // Certifique-se de que este arquivo está na pasta 'views'
-    } catch (err) {
-        console.error(err.message);
-        res.status(500).send('Erro no servidor');
-    }
-}); */
 
 app.get("/en", async (req, res) => {
   try {
@@ -61,14 +42,13 @@ app.get("/en", async (req, res) => {
       destino.atrativos = atrativosResult.rows;
     }
 
-    res.render("en", { destinos: destinos });
+    res.render("en", { destinos });
   } catch (err) {
     console.error(err.message);
     res.status(500).send("Erro no servidor");
   }
 });
 
-// Rota para espanhol
 app.get("/es", async (req, res) => {
   try {
     const destinosResult = await pool.query("SELECT * FROM Destino");
@@ -82,7 +62,31 @@ app.get("/es", async (req, res) => {
       destino.atrativos = atrativosResult.rows;
     }
 
-    res.render("es", { destinos: destinos }); // Certifique-se de que este arquivo está na pasta 'views'
+    res.render("es", { destinos });
+  } catch (err) {
+    console.error(err.message);
+    res.status(500).send("Erro no servidor");
+  }
+});
+
+app.get("/search", async (req, res) => {
+  const searchQuery = req.query.q;
+  try {
+    const destinosResult = await pool.query(
+      "SELECT * FROM Destino WHERE nome ILIKE $1 OR descricao ILIKE $1",
+      [`%${searchQuery}%`]
+    );
+
+    const destinos = destinosResult.rows;
+    for (let destino of destinos) {
+      const atrativosResult = await pool.query(
+        "SELECT * FROM Atrativo WHERE destino_id = $1 AND (nome ILIKE $2 OR descricao ILIKE $2)",
+        [destino.id, `%${searchQuery}%`]
+      );
+      destino.atrativos = atrativosResult.rows;
+    }
+
+    res.render("search", { destinos, query: searchQuery });
   } catch (err) {
     console.error(err.message);
     res.status(500).send("Erro no servidor");
@@ -91,32 +95,4 @@ app.get("/es", async (req, res) => {
 
 app.listen(port, () => {
   console.log(`Server running on port ${port}`);
-});
-
-
-
-//buscar no banco de dados
-
-app.get("/search", async (req, res) => {
-  const searchQuery = req.query.q;
-  try {
-    const destinosResult = await pool.query(
-      `SELECT * FROM Destino WHERE nome ILIKE $1 OR descricao ILIKE $1`,
-      [`%${searchQuery}%`]
-    );
-    
-    const destinos = destinosResult.rows;
-    for (let destino of destinos) {
-      const atrativosResult = await pool.query(
-        `SELECT * FROM Atrativo WHERE destino_id = $1 AND (nome ILIKE $2 OR descricao ILIKE $2)`,
-        [destino.id, `%${searchQuery}%`]
-      );
-      destino.atrativos = atrativosResult.rows;
-    }
-
-    res.render("search", { destinos: destinos, query: searchQuery });
-  } catch (err) {
-    console.error(err.message);
-    res.status(500).send("Erro no servidor");
-  }
 });
--- db.js
+++ db.js
@@ -1,12 +1,20 @@
 const { Pool } = require('pg');
 
-const connectionString = 'postgres://avnadmin:AVNS_tswV_5idt54dxgbDiib@desafio-v-desafiov.i.aivencloud.com:28847/defaultdb?';
+const connectionString = 'postgres://avnadmin:AVNS_tswV_5idt54dxgbDiib@desafio-v-desafiov.i.aivencloud.com:28847/defaultdb';
 
 const pool = new Pool({
-  connectionString: connectionString,
+  connectionString,
   ssl: {
     rejectUnauthorized: false,
   }
+});
+
+pool.connect((err, client, release) => {
+  if (err) {
+    return console.error('Error acquiring client', err.stack);
+  }
+  console.log('Connected to the database');
+  release();
 });
 
 module.exports = pool;
