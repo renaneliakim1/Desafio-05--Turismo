2e314cc61be20ed5b4e9a0fa99fe8640fe1d2d9a
renaneliakim1 <renaneliakim1@gmail.com>
2024-07-31 17:22:57 -0300

    .

--- app.js
+++ app.js
@@ -93,6 +93,12 @@ app.get("/search", async (req, res) => {
   }
 });
 
-app.listen(port, () => {
+/* app.listen(port, () => {
+  console.log(`Server running on port ${port}`);
+}); */
+
+
+
+app.listen(port, '0.0.0.0', () => {
   console.log(`Server running on port ${port}`);
 });
