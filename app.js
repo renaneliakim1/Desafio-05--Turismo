ac45d90fc5cc9c37f3890ccfda0f4927be37e119
renaneliakim1 <renaneliakim1@gmail.com>
2024-07-31 18:07:15 -0300

    trying to develop similarity string search

--- app.js
+++ app.js
@@ -69,19 +69,27 @@ app.get("/es", async (req, res) => {
   }
 });
 
+
+
+
+// realiza bvusca na base de dados por similaridade de strings
 app.get("/search", async (req, res) => {
   const searchQuery = req.query.q;
   try {
     const destinosResult = await pool.query(
-      "SELECT * FROM Destino WHERE nome ILIKE $1 OR descricao ILIKE $1",
-      [`%${searchQuery}%`]
+      `SELECT * FROM Destino 
+       WHERE similarity(nome, $1) > 0.2 OR similarity(descricao, $1) > 0.2
+       ORDER BY GREATEST(similarity(nome, $1), similarity(descricao, $1)) DESC`,
+      [searchQuery]
     );
 
     const destinos = destinosResult.rows;
     for (let destino of destinos) {
       const atrativosResult = await pool.query(
-        "SELECT * FROM Atrativo WHERE destino_id = $1 AND (nome ILIKE $2 OR descricao ILIKE $2)",
-        [destino.id, `%${searchQuery}%`]
+        `SELECT * FROM Atrativo 
+         WHERE destino_id = $1 AND (similarity(nome, $2) > 0.2 OR similarity(descricao, $2) > 0.2)
+         ORDER BY GREATEST(similarity(nome, $2), similarity(descricao, $2)) DESC`,
+        [destino.id, searchQuery]
       );
       destino.atrativos = atrativosResult.rows;
     }
@@ -92,6 +100,10 @@ app.get("/search", async (req, res) => {
     res.status(500).send("Erro no servidor");
   }
 });
+
+
+
+
 
 /* app.listen(port, () => {
   console.log(`Server running on port ${port}`);
